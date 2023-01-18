50510aadd8b3ddd0169e21cf7bbdca37545a41c1
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2023-01-18 11:07:13 -0300

    feat: :sparkles: Database Setup and Migrations

---
+++ prisma/migrations/20230115201933_init_migration/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT,
+    "avatar" TEXT,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_user_id_key" ON "Profile"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
